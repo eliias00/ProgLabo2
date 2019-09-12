84420d04072aca681f9b9712db7c9c12259d7e57
alumno <alumno@utnfra.com.ar>
2019-09-12 20:07:59 -0300

    .

---
+++ Ejercicio28/Ejercicio28/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Ejercicio28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTxtBox = new System.Windows.Forms.RichTextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTxtBox
+            // 
+            this.richTxtBox.Location = new System.Drawing.Point(29, 12);
+            this.richTxtBox.Name = "richTxtBox";
+            this.richTxtBox.Size = new System.Drawing.Size(514, 356);
+            this.richTxtBox.TabIndex = 0;
+            this.richTxtBox.Text = "";
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(443, 392);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(100, 31);
+            this.calcular.TabIndex = 1;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 450);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.richTxtBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxtBox;
+        private System.Windows.Forms.Button calcular;
+    }
+}
+
---
+++ Ejercicio28/Ejercicio28/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static int OrdenarDescendente(int num1, int num2)
+        {
+            return -num1.CompareTo(num2);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> miDiccionarioPalabras = new Dictionary<string, int>();
+
+            List<KeyValuePair<string, int>> dicLista = new List<KeyValuePair<string, int>>();
+
+            StringBuilder s = new StringBuilder();
+
+            string[] palabras = richTxtBox.Text.Split(new char[] { ' ', '.', ',', ';', ':', '!', '?', '(', ')' });
+
+
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                    
+                if (!miDiccionarioPalabras.ContainsKey(palabras[i]))
+                  {
+                    miDiccionarioPalabras.Add(palabras[i], 1);
+                  }
+                else
+                  {
+                    miDiccionarioPalabras[palabras[i]]++;
+                  }
+            }
+           
+            dicLista = miDiccionarioPalabras.ToList();
+            dicLista.Sort((x, y) => Ejercicio28.Form1.OrdenarDescendente(x.Value, y.Value));
+          
+            s.AppendLine("Palabras con mas apariciones: ");
+            for (int i = 0; i < dicLista.Capacity; i++)
+            {
+                s.AppendFormat("* {0} - apariciones: {1}\n", dicLista[i].Key, dicLista[i].Value);
+            }
+            MessageBox.Show(s.ToString(), "TOP 3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
